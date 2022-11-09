cd112950ad5275fb2e98a7f2924389ad6098c3ca
Taher027 <abutaherh27@gmail.com>
2022-11-09 19:27:48 +0600

    find single data by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middle ware 
@@ -17,13 +17,35 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-        
+        const serviceCollection = client.db('artSnap').collection('services');
+        app.get('/home', async (req, res) => {
+            const query = {};
+            const limit = 3;
+            const cursor = serviceCollection.find(query).limit(limit);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const service = await serviceCollection.findOne(query)
+            console.log(service);
+            res.send(service)
+        })
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
     }
-    catch {
-        
+    finally {
+
     }
 }
-run().catch(err => console.log(err.type, err.message ))
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('server is running')
